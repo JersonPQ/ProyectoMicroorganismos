0ec889e6d78be7fa10d5eefe8b83d58bfd851822
Drio02 <90213776+Drio02@users.noreply.github.com>
2023-03-24 17:30:54 -0600

    Listo moverse

--- src/Interface.java
+++ src/Interface.java
@@ -23,6 +23,7 @@ public class Interface {
 
         addComponentes();
 
+        ventana.setFocusable(true);
         ventana.pack();
         ventana.setVisible(true);
     }
@@ -63,85 +64,32 @@ public class Interface {
             }
         }
 
-        (dimensionMatriz[miMapa.getOrganismos()[0].getPosicion()[0]][miMapa.getOrganismos()[0].getPosicion()[1]]).boton.addKeyListener(new KeyAdapter() {
-            public void keyTyped(KeyEvent e){
-                Casilla casillaJugador = (dimensionMatriz[miMapa.getOrganismos()[0].getPosicion()[0]][miMapa.getOrganismos()[0].getPosicion()[1]]);
-                char codigo = e.getKeyChar();
-                if(codigo == 'a' || codigo == 'A'){
-                    System.out.println("Izquierda");
-                    casillaJugador.setObjeto();
-                    casillaJugador.boton.setIcon(null);
-                }
-                else if(codigo == 'd' || codigo == 'D'){
-                    System.out.println("Derecha");
-                    casillaJugador.setObjeto();
-                    casillaJugador.boton.setIcon(null);
-                }
-                else if(codigo == 'w' || codigo == 'W'){
-                    System.out.println("Arriba");
-                    casillaJugador.setObjeto();
-                    casillaJugador.boton.setIcon(null);
-                }
-                else if(codigo == 's' || codigo == 'S'){
-                    System.out.println("Abajo");
-                    casillaJugador.setObjeto();
-                    casillaJugador.boton.setIcon(null);
-                }
-            }
-        });
-
-
         ventana.add(mapaPanel);
     }
 
-
-
-
-
-
-
-
     //Crear la clase KeyListenes
-    class OyenteTeclado implements KeyListener{
-        
-        //Obteniendo el org jugador para poder moverlo con las teclas
-        Organismo orgJugador = miMapa.getOrganismos()[0];
-        ImageIcon imagenJugador = orgJugador.setImagen();
+    class OyenteTeclado extends KeyAdapter{
+            //Inicializamos los eventos
+            public void keyTyped(KeyEvent e){
+                
+            //Obteniendo el org jugador para poder moverlo con las teclas
+            Organismo orgJugador = miMapa.getOrganismos()[0];
+            ImageIcon imagenJugador = orgJugador.setImagen();
 
-        //Obtenes la posicion del jugador
-        int posJugadorX = orgJugador.getPosicion()[0];
-        int posJugadorY = orgJugador.getPosicion()[1];
+            //Obtenes la posicion del jugador
+            int posJugadorX = orgJugador.getPosicion()[0];
+            int posJugadorY = orgJugador.getPosicion()[1];
 
-        //Casilla donde se encuentra el organismoJugador
-        Casilla[][] mapaDimension = miMapa.getDimension();
-        Casilla casillaJugador = mapaDimension[posJugadorX][posJugadorY];
-        
-        //Inicializamos los eventos
-        public void keyTyped(KeyEvent e){
-            System.out.println("Hola");
-            int codigo = e.getKeyCode();
+            //Casilla donde se encuentra el organismoJugador
+            Casilla[][] mapaDimension = miMapa.getDimension();
+            Casilla casillaJugador = mapaDimension[posJugadorX][posJugadorY];
+            char codigo = e.getKeyChar();
+            
             //Programar para que el jugador se mueva
-            if(codigo == 87 || codigo == 119){
+            if(codigo == 'w' || codigo == 'W'){
                 //En este if la casilla se moverá para arriba
-                if(posJugadorY <= 0){
-                    //Aqui se tira la advertencia de que no se puede mover para la arriba
-                    JOptionPane.showMessageDialog(null, "No se puede mover para esa dirección");
-                }
-                else{
-                    Casilla siguiCasilla = mapaDimension[posJugadorX][posJugadorY-1];       
-                    casillaJugador.boton.setIcon(null); //Le quitamos la imagen
-                    casillaJugador.setObjeto(); //Le quitamos el objeto y lo desabilitamos
-
-                    //Colocar el objeto en las otra casilla y la imagen
-                    siguiCasilla.setObjeto(orgJugador);
-                    siguiCasilla.boton.setIcon(new ImageIcon(imagenJugador.getImage().getScaledInstance(15, 15, Image.SCALE_SMOOTH)));
-                    orgJugador.setPosition(posJugadorX, posJugadorY-1);
-                }
-            }
-            else if(codigo == 65 || codigo == 97){
-                //En este if la casilla se moverá para la izquierda
                 if(posJugadorX <= 0){
-                    //Aqui se tira la advertencia de que no se puede mover para la izquierda
+                    //Aqui se tira la advertencia de que no se puede mover para la arriba
                     JOptionPane.showMessageDialog(null, "No se puede mover para esa dirección");
                 }
                 else{
@@ -155,10 +103,44 @@ public class Interface {
                     orgJugador.setPosition(posJugadorX-1, posJugadorY);
                 }
             }
-            else if(codigo == 83 || codigo == 115){
+            else if(codigo == 'a' || codigo == 'A'){
+                //En este if la casilla se moverá para la izquierda
+                if(posJugadorY <= 0){
+                    //Aqui se tira la advertencia de que no se puede mover para la izquierda
+                    JOptionPane.showMessageDialog(null, "No se puede mover para esa dirección");
+                }
+                else{
+                    Casilla siguiCasilla = mapaDimension[posJugadorX][posJugadorY-1];       
+                    casillaJugador.boton.setIcon(null); //Le quitamos la imagen
+                    casillaJugador.setObjeto(); //Le quitamos el objeto y lo desabilitamos
+
+                    //Colocar el objeto en las otra casilla y la imagen
+                    siguiCasilla.setObjeto(orgJugador);
+                    siguiCasilla.boton.setIcon(new ImageIcon(imagenJugador.getImage().getScaledInstance(15, 15, Image.SCALE_SMOOTH)));
+                    orgJugador.setPosition(posJugadorX, posJugadorY-1);
+                }
+            }
+            else if(codigo == 's' || codigo == 'S'){
                 //En este se movera para abajo
-                if(posJugadorY >= 49){
+                if(posJugadorX >= 49){
                     //Aqui se tira la advertencia de que no se puede mover para abajo
+                    JOptionPane.showMessageDialog(null, "No se puede mover para esa dirección");
+                }
+                else{
+                    Casilla siguiCasilla = mapaDimension[posJugadorX+1][posJugadorY];       
+                    casillaJugador.boton.setIcon(null); //Le quitamos la imagen
+                    casillaJugador.setObjeto(); //Le quitamos el objeto y lo desabilitamos
+
+                    //Colocar el objeto en las otra casilla y la imagen
+                    siguiCasilla.setObjeto(orgJugador);
+                    siguiCasilla.boton.setIcon(new ImageIcon(imagenJugador.getImage().getScaledInstance(15, 15, Image.SCALE_SMOOTH)));
+                    orgJugador.setPosition(posJugadorX+1, posJugadorY);
+                }
+            }
+            else if(codigo == 'd' || codigo == 'D'){
+                //En este se movera para la derecha
+                if(posJugadorY >= 49){
+                    //Aqui se tira la advertencia de que no se puede mover para la derecha
                     JOptionPane.showMessageDialog(null, "No se puede mover para esa dirección");
                 }
                 else{
@@ -172,32 +154,6 @@ public class Interface {
                     orgJugador.setPosition(posJugadorX, posJugadorY+1);
                 }
             }
-            else if(codigo == 68 || codigo == 100){
-                //En este se movera para la derecha
-                if(posJugadorX >= 49){
-                    //Aqui se tira la advertencia de que no se puede mover para la derecha
-                    JOptionPane.showMessageDialog(null, "No se puede mover para esa dirección");
-                }
-                else{
-                    Casilla siguiCasilla = mapaDimension[posJugadorX+1][posJugadorX];       
-                    casillaJugador.boton.setIcon(null); //Le quitamos la imagen
-                    casillaJugador.setObjeto(); //Le quitamos el objeto y lo desabilitamos
-
-                    //Colocar el objeto en las otra casilla y la imagen
-                    siguiCasilla.setObjeto(orgJugador);
-                    siguiCasilla.boton.setIcon(new ImageIcon(imagenJugador.getImage().getScaledInstance(15, 15, Image.SCALE_SMOOTH)));
-                    orgJugador.setPosition(posJugadorX+1, posJugadorY);
-                }
-            }
-        }
-
-        //Este es el que vamos a usar
-        public void keyPressed(KeyEvent e){
-
-        }
-
-        public void keyReleased(KeyEvent e){
-
         }
     }
 }
